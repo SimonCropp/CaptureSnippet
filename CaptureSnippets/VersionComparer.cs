f7e8b06052cad3c362502aa782e3a5a3f70b0b42
Simon Cropp <simon.cropp@gmail.com>
2015-05-09 12:47:56 +1000

    better sorting

--- CaptureSnippets/VersionComparer.cs
+++ CaptureSnippets/VersionComparer.cs
@@ -8,29 +8,29 @@ namespace CaptureSnippets
         public static VersionComparer Instance = new VersionComparer();
         public int Compare(Version version1, Version version2)
         {
-            Guard.AgainstNull(version1, "version1");
-            Guard.AgainstNull(version2, "version2");
-            if (version1 == Version.ExplicitEmpty)
-            {
-                throw new ArgumentException("Cannot compare an ExplicitEmpty version.", "version1");
-            }
+            Guard.AgainstNull(version2, "version1");
+            Guard.AgainstNull(version1, "version2");
             if (version2 == Version.ExplicitEmpty)
             {
                 throw new ArgumentException("Cannot compare an ExplicitEmpty version.", "version2");
             }
-            if (version2.Major != version1.Major)
+            if (version1 == Version.ExplicitEmpty)
             {
-                return version2.Major > version1.Major ? 1 : -1;
+                throw new ArgumentException("Cannot compare an ExplicitEmpty version.", "version1");
             }
-            if (version2.Minor != version1.Minor)
+            if (version1.Major != version2.Major)
             {
-                return version2.Minor > version1.Minor ? 1 : -1;
+                return version1.Major > version2.Major ? 1 : -1;
             }
-            if (version2.Patch == version1.Patch)
+            if (version1.Minor != version2.Minor)
+            {
+                return version1.Minor > version2.Minor ? 1 : -1;
+            }
+            if (version1.Patch == version2.Patch)
             {
                 return 0;
             }
-            return version2.Patch > version1.Patch ? 1 : -1;
+            return version1.Patch > version2.Patch ? 1 : -1;
         }
 
     }
