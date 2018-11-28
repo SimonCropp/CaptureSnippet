0e90793a0b7968cbe6f144fa8d22b39e802edcf0
simoncropp <simon.cropp@gmail.com>
2018-11-29 10:03:28 +1100

    Update ModuleInitializer.cs

--- Tests/ModuleInitializer.cs
+++ Tests/ModuleInitializer.cs
@@ -4,9 +4,12 @@ public static class ModuleInitializer
 {
     public static void Initialize()
     {
-        var converters = ObjectApprover.JsonSerializer.Converters;
-        converters.Add(new VersionRangeConverter());
-        converters.Add(new ProcessResultConverter());
-        converters.Add(new SnippetConverter());
+        SerializerBuilder.ExtraSettings = settings =>
+        {
+            var converters = settings.Converters;
+            converters.Add(new VersionRangeConverter());
+            converters.Add(new ProcessResultConverter());
+            converters.Add(new SnippetConverter());
+        };
     }
 }
