a3218587dd70e65fcc0b70ed8d7dc3629d6341e1
simoncropp <simon.cropp@gmail.com>
2018-12-21 23:13:54 +1100

    .

--- CaptureSnippetsSimple.Tests/Scrubber.cs
+++
@@ -1,9 +0,0 @@
-﻿public static class Scrubber
-{
-    public static string Scrub(string s)
-    {
-        var testDirectory = AssemblyLocation.CurrentDirectory;
-        return s.Replace(@"\\", @"\")
-            .ReplaceCaseless(testDirectory, @"root\");
-    }
-}
