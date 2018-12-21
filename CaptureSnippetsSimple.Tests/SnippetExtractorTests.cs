7d453411a2cfa8467987136f934e57659be485b6
simoncropp <simon.cropp@gmail.com>
2018-12-21 22:34:37 +1100

    .

--- CaptureSnippetsSimple.Tests/SnippetExtractorTests.cs
+++ CaptureSnippetsSimple.Tests/SnippetExtractorTests.cs
@@ -41,23 +41,6 @@ public class SnippetExtractorTests : TestBase
         ObjectApprover.VerifyWithJson(snippets);
     }
 
-
-    [Fact]
-    public void Differ_by_version_missing_suffix()
-    {
-        var input = @"
-  <!-- startcode CodeKey 2-->
-  <configSections/>
-  <!-- endcode -->
-  <!-- startcode CodeKey 2.0-->
-  <configSections/>
-  <!-- endcode -->";
-        var snippets = FromText(input);
-        ObjectApprover.VerifyWithJson(snippets);
-    }
-
-
-
     [Fact]
     public void CanExtractWithInnerWhiteSpace()
     {
@@ -148,21 +131,6 @@ public class SnippetExtractorTests : TestBase
     }
 
     [Fact]
-    public void CanExtractMultipleWithDifferentVersions()
-    {
-        var input = @"
-  <!-- startcode CodeKey 4 -->
-  <configSections/>
-  <!-- endcode -->
-  #region CodeKey 5
-  The Code
-  #endregion";
-        var snippets = FromText(input);
-        ObjectApprover.VerifyWithJson(snippets);
-    }
-
-
-    [Fact]
     public void CanExtractFromXml()
     {
         var input = @"
@@ -171,22 +139,6 @@ public class SnippetExtractorTests : TestBase
   <!-- endcode -->";
         var snippets = FromText(input);
         ObjectApprover.VerifyWithJson(snippets);
-    }
-
-    [Fact]
-    public void CanExtractVersionFromFile()
-    {
-        var input = @"
-  <!-- startcode CodeKey -->
-  <configSections/>
-  <!-- endcode -->";
-        using (var stringReader = new StringReader(input))
-        {
-            var versionRange = new VersionRange(new NuGetVersion(1, 1, 0));
-            var extractor = FileSnippetExtractor.Build(versionRange, "package", false);
-            var snippets = extractor.AppendFromReader(stringReader, "path.cs").ToList();
-            ObjectApprover.VerifyWithJson(snippets.Single());
-        }
     }
 
     public List<Snippet> FromText(string contents)
@@ -199,53 +151,10 @@ public class SnippetExtractorTests : TestBase
     }
 
     [Fact]
-    public void CanExtractFromXmlWithVersion()
-    {
-        var input = @"
-  <!-- startcode CodeKey 5 -->
-  <configSections/>
-  <!-- endcode -->";
-        var snippets = FromText(input);
-        ObjectApprover.VerifyWithJson(snippets);
-    }
-
-    [Fact]
-    public void CanExtractFromAllVersion()
-    {
-        var input = @"
-  <!-- startcode CodeKey 1 -->
-  <configSections/>
-  <!-- endcode -->";
-        var snippets = FromText(input);
-        ObjectApprover.VerifyWithJson(snippets);
-    }
-
-    [Fact]
-    public void CanExtractFromXmlWithVersionRange()
-    {
-        var input = @"
-  <!-- startcode CodeKey [1.0,2.0] -->
-  <configSections/>
-  <!-- endcode -->";
-        var snippets = FromText(input);
-        ObjectApprover.VerifyWithJson(snippets);
-    }
-
-    [Fact]
     public void UnClosedSnippet()
     {
         var input = @"
   <!-- startcode CodeKey -->
-  <configSections/>";
-        var snippets = FromText(input);
-        ObjectApprover.VerifyWithJson(snippets);
-    }
-
-    [Fact]
-    public void UnClosedSnippetWithVersion()
-    {
-        var input = @"
-  <!-- startcode CodeKey 5 -->
   <configSections/>";
         var snippets = FromText(input);
         ObjectApprover.VerifyWithJson(snippets);
@@ -262,31 +171,10 @@ public class SnippetExtractorTests : TestBase
     }
 
     [Fact]
-    public void UnClosedRegionWithVersion()
-    {
-        var input = @"
-  #region CodeKey 5
-  <configSections/>";
-        var snippets = FromText(input);
-        ObjectApprover.VerifyWithJson(snippets);
-    }
-
-    [Fact]
     public void CanExtractFromRegion()
     {
         var input = @"
   #region CodeKey
-  The Code
-  #endregion";
-        var snippets = FromText(input);
-        ObjectApprover.VerifyWithJson(snippets);
-    }
-
-    [Fact]
-    public void CanExtractFromRegionWithVersion()
-    {
-        var input = @"
-  #region CodeKey 5
   The Code
   #endregion";
         var snippets = FromText(input);
@@ -305,32 +193,10 @@ public class SnippetExtractorTests : TestBase
     }
 
     [Fact]
-    public void CanExtractWithNoTrailingCharactersWithVersion()
-    {
-        var input = @"
-  // startcode CodeKey 6
-  the code
-  // endcode ";
-        var snippets = FromText(input);
-        ObjectApprover.VerifyWithJson(snippets);
-    }
-
-    [Fact]
     public void CanExtractWithMissingSpaces()
     {
         var input = @"
   <!--startcode CodeKey-->
-  <configSections/>
-  <!--endcode-->";
-        var snippets = FromText(input);
-        ObjectApprover.VerifyWithJson(snippets);
-    }
-
-    [Fact]
-    public void CanExtractWithMissingSpacesWithVersion()
-    {
-        var input = @"
-  <!--startcode CodeKey 6-->
   <configSections/>
   <!--endcode-->";
         var snippets = FromText(input);
@@ -347,17 +213,4 @@ public class SnippetExtractorTests : TestBase
         var snippets = FromText(input);
         ObjectApprover.VerifyWithJson(snippets);
     }
-
-    [Fact]
-    public void CanExtractWithTrailingWhitespaceWithVersion()
-    {
-        var input = @"
-  // startcode CodeKey 4
-  the code
-  // endcode   ";
-        var snippets = FromText(input);
-        ObjectApprover.VerifyWithJson(snippets);
-    }
-
-
 }
