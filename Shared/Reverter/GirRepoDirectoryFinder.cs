14be696d30e9dd12315cbc4cdf263e8f73196cb9
simoncropp <simon.cropp@gmail.com>
2018-12-23 14:50:00 +1100

    add git finder

---
+++ Shared/Reverter/GirRepoDirectoryFinder.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+
+namespace CaptureSnippets
+{
+    public static class GirRepoDirectoryFinder
+    {
+        public static bool TryFind(out string path)
+        {
+            var currentDirectory = AssemblyLocation.CurrentDirectory;
+            do
+            {
+                if (Directory.Exists(Path.Combine(currentDirectory, ".git")))
+                {
+                    path = currentDirectory;
+                    return true;
+                }
+
+                var parent = Directory.GetParent(currentDirectory);
+                if (parent == null)
+                {
+                    path = null;
+                    return false;
+                }
+
+                currentDirectory = parent.FullName;
+            } while (true);
+        }
+    }
+}
---
+++ TestsShared/Reverter/GirRepoDirectoryFinderTests.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using CaptureSnippets;
+using Xunit;
+
+public class GirRepoDirectoryFinderTests
+{
+    [Fact]
+    public void CanFindGirRepoDir()
+    {
+        Assert.True(GirRepoDirectoryFinder.TryFind(out var path));
+        Assert.True(Directory.Exists(path));
+    }
+}
